b57b0a305cce62db090c686c3ac7719f455b49f5
martinforsyth <martin@forfamily.co.uk>
2018-12-13 10:29:34 +0000

    committing from hp8200

---
+++ hp8200/emacs/emacs-customizations.el
@@ -0,0 +1,32 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 (quote
+		(magit hideshowvis frame-cmds yasnippet diminish json-mode solarized-theme powerline popup flycheck-pos-tip flycheck-irony flycheck-color-mode-line flycheck company-tern company-irony company irony-eldoc irony psvn dash-functional dash s pkg-info epl rtags multi-web-mode web-mode web-mode-edit-element bm)))
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
+
+
+;; nice font, from (describe-font) ...
+;; 
+;; name (opened by): -PfEd-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1
+;;        full name: DejaVu Sans Mono:pixelsize=13:foundry=PfEd:weight=normal:slant=normal:width=normal:spacing=100:scalable=true
+;;        file name: /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf
+;;             size: 13
+;;           height: 17
+;;  baseline-offset:  0
+;; relative-compose:  0
+;;   default-ascent:  0
+;;           ascent: 13
+;;          descent:  4
+;;    average-width:  8
+;;      space-width:  8
+;;        max-width:  8
