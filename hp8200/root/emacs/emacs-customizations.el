52b4da86d207216970444a4b4c063a68d13dcee9
martinforsyth <martin@forfamily.co.uk>
2019-01-15 14:03:37 +0000

    add root files and remove smart line mode

---
+++ hp8200/root/emacs/emacs-customizations.el
@@ -0,0 +1,112 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+	 [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+	 ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-safe-themes
+	 (quote
+		("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+	 (--map
+		(solarized-color-blend it "#002b36" 0.25)
+		(quote
+		 ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+	 (quote
+		(("#073642" . 0)
+		 ("#546E00" . 20)
+		 ("#00736F" . 30)
+		 ("#00629D" . 50)
+		 ("#7B6000" . 60)
+		 ("#8B2C02" . 70)
+		 ("#93115C" . 85)
+		 ("#073642" . 100))))
+ '(hl-bg-colors
+	 (quote
+		("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+	 (quote
+		("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+ '(magit-diff-use-overlays nil)
+ '(make-backup-files nil)
+ '(nrepl-message-colors
+	 (quote
+		("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(package-selected-packages
+	 (quote
+		(phpactor phpstan psysh phpunit composer composer\.el php-runtime php-mode js2-refactor js2-mode magit hideshowvis frame-cmds yasnippet diminish json-mode solarized-theme powerline popup flycheck-pos-tip flycheck-irony flycheck-color-mode-line flycheck company-tern company-irony company irony-eldoc irony psvn dash-functional dash s pkg-info epl rtags multi-web-mode web-mode web-mode-edit-element bm)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(show-paren-mode t)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(split-height-threshold 0)
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#dc322f")
+		 (40 . "#c8805d801780")
+		 (60 . "#bec073400bc0")
+		 (80 . "#b58900")
+		 (100 . "#a5008e550000")
+		 (120 . "#9d0091000000")
+		 (140 . "#950093aa0000")
+		 (160 . "#8d0096550000")
+		 (180 . "#859900")
+		 (200 . "#66aa9baa32aa")
+		 (220 . "#57809d004c00")
+		 (240 . "#48559e556555")
+		 (260 . "#392a9faa7eaa")
+		 (280 . "#2aa198")
+		 (300 . "#28669833af33")
+		 (320 . "#279993ccbacc")
+		 (340 . "#26cc8f66c666")
+		 (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+	 (quote
+		(unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83")))
+ '(which-function-mode t)
+ '(xterm-color-names
+	 ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+	 ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 98 :width normal)))))
+
+
+;; nice font, from (describe-font) ...
+;; 
+;; name (opened by): -PfEd-DejaVu Sans Mono-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1
+;;        full name: DejaVu Sans Mono:pixelsize=13:foundry=PfEd:weight=normal:slant=normal:width=normal:spacing=100:scalable=true
+;;        file name: /usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf
+;;             size: 13
+;;           height: 17
+;;  baseline-offset:  0
+;; relative-compose:  0
+;;   default-ascent:  0
+;;           ascent: 13
+;;          descent:  4
+;;    average-width:  8
+;;      space-width:  8
+;;        max-width:  8
---
+++ hp8200/root/emacs/emacs-local-keymaps.el
@@ -0,0 +1,60 @@
+;; ===========================
+;; key mappings
+;; ===========================
+;; Remember that a number of bindings are already 'taken' in
+;; k6/emacs/.emacs:(defun our-cmode-hook ()
+;;(global-set-key [?\C-\M-z] 'tv-get-mod)
+(global-set-key [C-kp-up] 'line-to-top-of-window)
+(global-set-key [f1] 'undo) ;; previously toggle-read-only
+;;(global-set-key [f2] 't2s) ;; previously ff-find-other-file
+;;(global-set-key [(f3)] (lambda () (interactive) (manual-entry (current-word))));; get man page for the library call at the current cursor position.
+;;(global-set-key [f4] ' bkb-my-nice-toggle-framewidth)
+(global-set-key (kbd "<C-M-insert>") 'bm-toggle)
+(global-set-key (kbd "<C-M-delete>") 'bm-remove-all)
+(global-set-key (kbd "<C-M-next>")   'bm-next)
+(global-set-key (kbd "<C-M-prior>") 'bm-previous)
+;;(global-set-key [?\C-c ?\C-c] 'comment-region)
+(global-set-key [?\C-c ?c] 'comment-region)
+;;(global-set-key (kbd "C-g") 'my-keybord-quit)
+
+;;(global-set-key (C-c <C-up>) 'mf-scroll-up)
+;;(global-set-key (C-c <C-down>) 'mf-scroll-down)
+
+;;(global-set-key [?\M-.] 'find-tag)
+;;(global-set-key [?\M-*] 'pop-tag-mark)
+;;(global-set-key [f7] 'cscope-find-functions-calling-this-function)
+;;(global-set-key [M-backspace] 'backward-kill-word)
+
+(global-set-key (kbd "C-c a") 'apropos)
+;;(global-set-key [?\C-c ?A] 'bkb-insert-wiki-analysis-headings)
+;;(global-set-key [?\C-c ?c] 'command-history)
+(global-set-key [?\C-c ?E] 'ediff-revision)
+(global-set-key [?\C-c ?e] 'vc-version-ediff)
+
+(global-set-key [?\C-c ?f] 'find-name-dired)
+;;(global-set-key [?\C-c ?f] 'uchar-string-literal-format);; temp
+
+;;(global-set-key [?\C-c ?g] 'bkb-trace-goto-source)
+(global-set-key [?\C-c ?h] 'hexl-find-file)
+;;(global-set-key [?\C-c ?m] 'bkb-insert-c-code-main)
+;;(global-set-key [?\C-c ?M] 'bkb-insert-makefile-simple)
+;;(global-set-key [?\C-c ?n] 'tv-forward-mod)
+(global-set-key [?\C-c ?o] 'occur)
+;;(global-set-key [?\C-c ?p] 'tv-backward-mod)
+(global-set-key [?\C-c ?r] 'rgrep)
+;;(global-set-key [?\C-c ?R] 'remember)
+;;(global-set-key [?\C-c ?T] 'bkb-tidy-frames)
+(global-set-key [?\C-c ?I] 'info-apropos)
+(global-set-key [?\C-c ?i] 'imenu-add-menubar-index)
+;;(global-set-key [?\C-c ?l] 'bkb-insert-trace)
+;;(global-set-key [?\C-c ?8] 'goto-line-over-80-chars)
+(global-set-key [?\C-c ?u] 'my-untabify)
+(global-set-key [?\C-c ?v] 'apropos-variable)
+;;(global-set-key [?\C-c ?x] 'bkb-compile-and-run)
+;;(global-set-key [?\C-c ?C] 'bkb-compile-and-run)
+
+;; keep exuberant ctags available for finding defs - at least until rtags is working nicely
+(global-set-key [?\C-c ?\M-.] 'find-tag)
+(global-set-key [?\C-c ?\M-\[] 'pop-tag-mark)
+
+(global-set-key [?\C-c ?T] 'indent-buffer)
